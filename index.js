d8624a940db45eef5f14c47711eaf20948d91397
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-02 19:41:57 +0900

    update devDependencies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const win32Resolve = pathLib.win32.resolve;
 const posixResolve = pathLib.posix.resolve;
 
 module.exports = function tildePath(path) {
-  if (process.platform === 'win32') {
-    return win32Resolve(path);
-  }
+	if (process.platform === 'win32') {
+		return win32Resolve(path);
+	}
 
-  const home = homedir();
-  path = posixResolve(path);
+	const home = homedir();
+	path = posixResolve(path);
 
-  if (path === home) {
-    return '~';
-  }
+	if (path === home) {
+		return '~';
+	}
 
-  const homeWithTrailingSlash = `${home}/`;
+	const homeWithTrailingSlash = `${home}/`;
 
-  if (path.startsWith(homeWithTrailingSlash)) {
-    return path.replace(homeWithTrailingSlash, '~/');
-  }
+	if (path.startsWith(homeWithTrailingSlash)) {
+		return path.replace(homeWithTrailingSlash, '~/');
+	}
 
-  return path;
+	return path;
 };
--- test.js
+++ test.js
@@ -8,71 +8,71 @@ const test = require('tape');
 const tildePath = require('.');
 
 test('tildePath()', t => {
-  t.throws(
-    () => tildePath(123),
-    /^TypeError.*Path must be a string. Received 123/,
-    'should throw a type error when it takes a non-string argument.'
-  );
+	t.throws(
+		() => tildePath(123),
+		/^TypeError.*Path must be a string. Received 123/,
+		'should throw a type error when it takes a non-string argument.'
+	);
 
-  t.throws(
-    () => tildePath(),
-    /^TypeError.*Path must be a string. Received undefined/,
-    'should throw a type error when it takes no arguments.'
-  );
+	t.throws(
+		() => tildePath(),
+		/^TypeError.*Path must be a string. Received undefined/,
+		'should throw a type error when it takes no arguments.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('tildePath() on a Windows environment', t => {
-  pretendPlatform('win32');
+	pretendPlatform('win32');
 
-  t.equal(
-    tildePath('foo'),
-    win32.resolve('foo'),
-    'should make a given path absolute without using tilde.'
-  );
+	t.equal(
+		tildePath('foo'),
+		win32.resolve('foo'),
+		'should make a given path absolute without using tilde.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('tildePath() on a non-Windows environment', t => {
-  pretendPlatform('aix');
-  process.chdir(homedir());
+	pretendPlatform('aix');
+	process.chdir(homedir());
 
-  t.equal(
-    tildePath('baz'),
-    '~/baz',
-    'should convert a relative path to an absolute tilde path.'
-  );
+	t.equal(
+		tildePath('baz'),
+		'~/baz',
+		'should convert a relative path to an absolute tilde path.'
+	);
 
-  t.equal(
-    tildePath(''),
-    '~',
-    'should accept an empty string.'
-  );
+	t.equal(
+		tildePath(''),
+		'~',
+		'should accept an empty string.'
+	);
 
-  const newHome = '/Users/__test__';
+	const newHome = '/Users/__test__';
 
-  process.env.HOME = newHome;
-  process.env.USERPROFILE = newHome;
+	process.env.HOME = newHome;
+	process.env.USERPROFILE = newHome;
 
-  t.equal(
-    tildePath(posix.join(`${newHome}/foo/bar`)),
-    '~/foo/bar',
-    'should convert an absolute path to a tilde path.'
-  );
+	t.equal(
+		tildePath(posix.join(`${newHome}/foo/bar`)),
+		'~/foo/bar',
+		'should convert an absolute path to a tilde path.'
+	);
 
-  t.equal(
-    tildePath(posix.resolve('/')),
-    posix.resolve('/'),
-    'should not modify the path if a string doesn\'t include user home directory.'
-  );
+	t.equal(
+		tildePath(posix.resolve('/')),
+		posix.resolve('/'),
+		'should not modify the path if a string doesn\'t include user home directory.'
+	);
 
-  t.equal(
-    tildePath(`${homedir()}abcdef`),
-    `${homedir()}abcdef`,
-    'should not modify the path if it\'s owned by another user.'
-  );
+	t.equal(
+		tildePath(`${homedir()}abcdef`),
+		`${homedir()}abcdef`,
+		'should not modify the path if it\'s owned by another user.'
+	);
 
-  t.end();
+	t.end();
 });
